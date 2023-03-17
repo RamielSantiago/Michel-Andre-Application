22145d0f6c9c7e7112adfee284a7654f05742715
Fuzzy-Fluffy-Cat <75292725+Fuzzy-Fluffy-Cat@users.noreply.github.com>
2023-03-18 04:19:40 +0800

    Add files via upload

---
+++ HRMS/HRMS/Presenter/OrgPresenter.cs
@@ -0,0 +1,20 @@
+﻿using HRMS.Model;
+using HRMS.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Presenter
+{
+    public class OrgPresenter
+    {
+        private readonly View.OrgChartView view;
+
+        public OrgPresenter(View.OrgChartView view)
+        {
+            this.view = view;
+        }
+    }
+}
